5ed17816a90c4f095676d2a4a2399ba2c8abbd91
marcio santos <marciosantosf7@gmail.com>
2018-09-20 02:55:15 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/msantos/agileshop/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.msantos.agileshop.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.msantos.agileshop.domain.Pedido;
+import com.msantos.agileshop.exceptions.ObjectNotFoundException;
+import com.msantos.agileshop.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		}
+}
