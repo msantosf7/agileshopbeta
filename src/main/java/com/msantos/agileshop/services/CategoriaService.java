43f328e552e15bfd9de1b2a7341fc028af00e8af
marcio santos <marciosantosf7@gmail.com>
2018-09-20 18:12:49 -0300

    Salvando uma nova Categoria com o POST

--- src/main/java/com/msantos/agileshop/services/CategoriaService.java
+++ src/main/java/com/msantos/agileshop/services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
+	
+	public Categoria salvar(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
