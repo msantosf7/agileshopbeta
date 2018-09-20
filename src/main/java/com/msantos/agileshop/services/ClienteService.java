b40a5815a1d93cadf7e61a33cb5a3f6a9f6ecb3b
marcio santos <marciosantosf7@gmail.com>
2018-09-20 00:43:04 -0300

    Endpoint /Clientes/{id}

---
+++ src/main/java/com/msantos/agileshop/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.msantos.agileshop.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.msantos.agileshop.domain.Cliente;
+import com.msantos.agileshop.exceptions.ObjectNotFoundException;
+import com.msantos.agileshop.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		}
+}
